eb96ab0e2b5eb71fab2e24f59efbccbc16a63404
Jack Stdin <hellotan@live.ru>
2017-08-14 17:48:34 +0300

    Rework logic

--- nats_helper/__init__.py
+++ nats_helper/__init__.py
@@ -1,10 +1,8 @@
 import asyncio
-import functools
 import signal
 import time
-from enum import Enum
 
-from nats.aio.client import Client as NatsClient, ErrTimeout
+from nats.aio.client import Client as NatsClient
 
 
 def require_connect_async(func):
@@ -16,40 +14,21 @@ def require_connect_async(func):
     return wrapper
 
 
-def require_connect(func):
+def require_connect_threadsafe(func):
     def wrapper(self, *args, **kwargs):
         if not self.connected and self._connect_params is not None:
-            self.connect(**self._connect_params)
-        if self._pre_ping:
-            try:
-                self._loop.run_until_complete(self._nc.flush(0.3))
-            except ErrTimeout:
-                self._log.info("Ping timeout, reconnect")
+            asyncio.run_coroutine_threadsafe(self.connect_async(**self._connect_params), self._loop).result()
 
-                async def wait_for_reconnect():
-                    while self._state != self.State.RECONNECTED:
-                        pass
-
-                self._loop.run_until_complete(wait_for_reconnect())
-            return func(self, *args, **kwargs)
-        else:
-            return func(self, *args, **kwargs)
+        return func(self, *args, **kwargs)
 
     return wrapper
 
 
 class NatsHelper(object):
     __slots__ = ['_nc', '_loop', '_log', '_name', '_connect_params', '_run_exclusive', '_reconnect_count',
-                 '_reconnect_timeout', '_subscriptions', '_pre_ping', '_state']
+                 '_reconnect_timeout', '_subscriptions', '_threaded']
 
-    class State(Enum):
-        NOT_CONNECTED = 0
-        CONNECTED = 1
-        DISCONNECTED = 2
-        RECONNECTED = 3
-
-    def __init__(self, loop, logger, name=None, connect_params=None, reconnect_count=10, reconnect_timeout=5,
-                 pre_ping=False):
+    def __init__(self, loop, logger, name=None, connect_params=None, reconnect_count=10, reconnect_timeout=5):
         """
         :param loop: Asyncio event loop 
         :param logger: logger instance, logging.getLogger(...)
@@ -60,17 +39,25 @@ class NatsHelper(object):
         self._loop = loop
         self._log = logger
         self._connect_params = connect_params
-        self._pre_ping = pre_ping
 
         self._reconnect_count = reconnect_count
         self._reconnect_timeout = reconnect_timeout
+        self._run_exclusive = None
+        self._threaded = False
 
         self._subscriptions = {}
-        self._state = self.State.NOT_CONNECTED
 
     @property
     def connected(self):
         return self._nc.is_connected
+
+    @property
+    def threaded(self):
+        return self._threaded
+
+    @threaded.setter
+    def threaded(self, value):
+        self._threaded = value
 
     async def connect_async(self, *args, **kwargs):
         # callbacks
@@ -79,6 +66,10 @@ class NatsHelper(object):
 
         async def close_cb():
             self._log.warning("connection to NATS closed.")
+
+            if self._nc.is_closed:
+                return
+
             if self._reconnect_count != 0 and self._connect_params is not None:
                 for i in range(1, self._reconnect_count):
                     try:
@@ -94,15 +85,13 @@ class NatsHelper(object):
                             await self._subscribe(*sub_params[0], **sub_params[1])
                         return
             else:
-                self.shutdown()
+                self.shutdown('')
 
         async def disconnected_cb():
             self._log.error("NATS disconnected!")
-            self._state = self.State.DISCONNECTED
 
         async def reconnected_cb():
             self._log.info("NATS reconnected!")
-            self._state = self.State.RECONNECTED
 
         options = {
             'name': self._name,
@@ -116,7 +105,6 @@ class NatsHelper(object):
 
         await self._nc.connect(**options)
         self._connect_params = kwargs
-        self._state = self.State.CONNECTED
 
     def connect(self, *args, **kwargs):
         """
@@ -143,69 +131,65 @@ class NatsHelper(object):
         kwargs['cb'] = subscriber
         self._loop.run_until_complete(self._subscribe(*args, **kwargs))
 
-    def start(self, exclusive=True):
-        for sig in ("SIGHUP", "SIGTERM", "SIGINT"):
-            self._loop.add_signal_handler(getattr(signal, sig), self.shutdown)
+    def start(self, exclusive=True, use_sig=True):
+        if use_sig:
+            for sig in ("SIGHUP", "SIGTERM", "SIGINT"):
+                self._loop.add_signal_handler(getattr(signal, sig), self.shutdown, sig)
 
         self._run_exclusive = exclusive
         if exclusive:
             self._loop.run_forever()
 
     # Signal handler
-    def shutdown(self):
+    def shutdown(self, sig):
         if self._run_exclusive is None:
             self._log.info("nats-helper isn't started!")
             return
 
-        if self._run_exclusive:
+        if not self._nc.is_closed:
+            if self._threaded:
+                asyncio.run_coroutine_threadsafe(self._nc.close(), self._loop)
+            else:
+                self._reconnect_count = 0
+                try:
+                    asyncio.get_event_loop().run_until_complete(self._nc.close())
+                except:
+                    pass
+        else:
+            self._log.info("nats-helper was already closed!")
+
+        if self._loop.is_running():
             self._log.info("nats-helper closing...")
             time.sleep(1)
-            self._loop.stop()
+            self._loop.call_soon_threadsafe(self._loop.stop)
             self._log.info("loop stopped, wait 1s for shutdown...")
             time.sleep(1)
             self._run_exclusive = None
 
-        if not self._nc.is_closed:
-            self._loop.create_task(self._nc.close())
-        else:
-            self._log.info("nats-helper was already closed!")
-
         self._log.info("bye!")
 
     # timed_request
-    @require_connect
-    def timed_request(self, *args, **kwargs):
-        return self._loop.run_until_complete(self._nc.timed_request(*args, **kwargs))
-
-    @require_connect
-    def timed_request_soon(self, *args, **kwargs):
-        return self._loop.call_soon(functools.partial(self._nc.timed_request, *args, **kwargs))
+    @require_connect_threadsafe
+    def timed_request_threadsafe(self, *args, **kwargs):
+        return asyncio.run_coroutine_threadsafe(self._nc.timed_request(*args, **kwargs), self._loop).result()
 
     @require_connect_async
     async def timed_request_async(self, *args, **kwargs):
         return await self._nc.timed_request(*args, **kwargs)
 
     # publish
-    @require_connect
-    def publish(self, *args, **kwargs):
-        return self._loop.run_until_complete(self._nc.publish(*args, **kwargs))
-
-    @require_connect
-    def publish_soon(self, *args, **kwargs):
-        return self._loop.call_soon(functools.partial(self._nc.publish, *args, **kwargs))
+    @require_connect_threadsafe
+    def publish_threadsafe(self, *args, **kwargs):
+        return asyncio.run_coroutine_threadsafe(self._nc.publish(*args, **kwargs), self._loop).result()
 
     @require_connect_async
     async def publish_async(self, *args, **kwargs):
         return await self._nc.publish(*args, **kwargs)
 
     # publish_request
-    @require_connect
-    def publish_request(self, *args, **kwargs):
-        return self._loop.run_until_complete(self._nc.publish_request(*args, **kwargs))
-
-    @require_connect
-    def publish_request_soon(self, *args, **kwargs):
-        return self._loop.call_soon(functools.partial(self._nc.publish_request, *args, **kwargs))
+    @require_connect_threadsafe
+    def publish_request_threadsafe(self, *args, **kwargs):
+        return asyncio.run_coroutine_threadsafe(self._nc.publish_request(*args, **kwargs), self._loop).result()
 
     @require_connect_async
     async def publish_request_async(self, *args, **kwargs):
